a53f804b8e10692297b7504ddc066d86c83baf26
znyadzi <znyadzi1@gmail.com>
2023-09-16 08:49:00 +0000

    just testing the git workout

--- Admin/Login/index.php
+++ Admin/Login/index.php
@@ -12,6 +12,7 @@ session_start();
 			<title>Admin | Login </title>
 		</head>
 		<body>
+			
 		<?php
 			if (isset($_SESSION['UserName']) && isset($_SESSION['Account_Type'])) {
 				echo"<script> alert('User Already Logged In'); window.location='../Dashboard'; </script>";
